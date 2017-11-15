3e21ec364d131cc42c6108ce666a2cc79ca62a7e
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-11-15 04:23:03 +0300

    Add CRI API version detection

---
+++ criproxy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+api_version=1.8
+version_str=
+if hash kubectl 2>/dev/null; then
+    version_str="$(kubectl --version)"
+elif hash hyperkube 2>/dev/null; then
+    version_str="$(hyperkube kubectl --version)"
+elif [[ -x /k8s/hyperkube ]]; then
+    # kubeadm-dind-cluster
+    version_str="$(/k8s/hyperkube kubectl --version)"
+fi
+    
+if [[ ${version_str} =~ v1\.7\. ]]; then
+    api_version=1.7
+fi
+
+exec /usr/bin/criproxy -apiVersion "${api_version}" "$@"
