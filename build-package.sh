4ceae9f48d274252925620114b12ed8371466dec
Ivan Shvedunov <ishvedunov@mirantis.com>
2017-11-20 11:07:32 +0300

    Generate debian changelog automatically

--- build-package.sh
+++ build-package.sh
@@ -9,7 +9,21 @@ if [[ ! -d vendor ]]; then
 fi
 
 go build 1>&2
+
+# https://www.debian.org/doc/manuals/maint-guide/update.en.html#idm3360
+date="$(LANG=C date -R)"
+version="$(git describe 2>/dev/null | sed 's/[^0-9.]//g' || true)"
+version="${version:-0.0.0}"
+author="Ivan Shvedunov <ishvedunov@mirantis.com>"
+
+cat >debian/changelog <<EOF
+criproxy (${version}) stable; urgency=optional
+
+  * https://github.com/Mirantis/criproxy
+
+ -- ${author}  ${date}
+
+EOF
 dpkg-buildpackage -us -uc -b 1>&2
 
 # (cd ../ && tar -c criproxy*deb)
-
